07f4b09cf7378368a59b2a0a373b3893a79e8b36
hissa77a <h.alotaibi@bub.edu.bh>
2026-08-02 16:07:06 +0300

    started on the js

--- js/app.js
+++ js/app.js
@@ -2,12 +2,27 @@
 
 
 /*---------- Variables (state) ---------*/
-
+let card1 = ""
+let card2 = ""
 
 /*----- Cached Element References  -----*/
 
+const restartBtn = document.querySelector('#restart-btn')
+const cards = document.querySelectorAll('.card')
+const elmntMoves = document.querySelector('#moves')
+const gameTimer = document.querySelector('#timer')
 
 /*-------------- Functions -------------*/
 
+function flipCard(event) {
+    const clickedCard = event.currentTarget;
+    clickedCard.classList.add('flipped')
+    console.log('clicked') //checks if function works
+}
 
 /*----------- Event Listeners ----------*/
+
+cards.forEach ((card) => {
+    card.addEventListener('click', flipCard)
+});
+
